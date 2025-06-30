a868b6ef391f63ff6b7f4d8e0ff372aaf3ac9028
Patryk Leszczynski <>
2025-06-30 12:27:48 +0200

    Add Terraform configuration for GCP providers and backend setup

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
 provider "google" {
   project      = var.gcp_project
   region       = var.gcp_region
+  
 }
 
 # GCP beta provider
---
+++ terraform/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "gcs" {
+    bucket  = "static-website-34296-terraform-state"
+    prefix  = "terraform/state"
+    
+  }
+}
