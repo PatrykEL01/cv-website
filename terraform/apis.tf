45bf14a26a3551ea7bbaf7c203f779cc0b22b436
Patryk Leszczynski <>
2025-07-07 13:30:15 +0200

    Add Cloud Resource Manager API configuration to Terraform setup

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -40,3 +40,8 @@ resource "google_project_service" "certificatemanager_api" {
 
   disable_on_destroy = false
 }
+
+resource "google_project_service" "enable_cloud_resource_manager_api" {
+  service                    = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
+}
